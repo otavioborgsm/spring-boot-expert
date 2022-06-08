734c5f80a1b568967a8a06321799e993f10e1cba
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-08 15:57:07 -0300

    multiplos ambientes e profiles

--- vendas/src/main/java/io/github/otavioborgsm/MyConfiguration.java
+++ vendas/src/main/java/io/github/otavioborgsm/MyConfiguration.java
@@ -1,10 +1,17 @@
 package io.github.otavioborgsm;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
-
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args ->{
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
+    }
 }
