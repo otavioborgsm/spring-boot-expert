5d2744edae7b6bd0c0b63aa6a1c6fd40bb96babf
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-09 15:32:03 -0300

    salvando e recuperando clientes

--- vendas/src/main/resources/data.sql
+++ vendas/src/main/resources/data.sql
@@ -1,24 +1,24 @@
-create table cliente(
-    id integer primary key auto_increment,
-    nome varchar(100)
+CREATE TABLE CLIENTE(
+    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    NOME VARCHAR(100)
 );
 
-create table produto(
-    id integer primary key auto_increment,
-    descricao varchar(100),
-    preco_unitario numeric(20,2)
+CREATE TABLE PRODUTO(
+    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    DESCRICAO VARCHAR(100),
+    PRECO_UNITARIO NUMERIC(20,2)
 );
 
 create table pedido(
-    id integer primary key auto_increment,
-    cliente_id integer references cliente (id),
-    data_pedido timestamp,
-    total numeric(20,2)
+    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    CLIENTE_ID INTEGER REFERENCES CLIENTE (ID),
+    DATA_PEDIDO TIMESTAMP,
+    TOTAL NUMERIC(20,2)
 );
 
 create table item_pedido(
-    id integer primary key auto_increment,
-    pedido_id integer references pedido (id),
-    produto_id integer references produto (id),
-    quantidade integer
+    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    PEDIDO_ID INTEGER REFERENCES PEDIDO (ID),
+    PRODUTO_ID INTEGER REFERENCES PRODUTO (ID),
+    QUANTIDADE INTEGER
 );
