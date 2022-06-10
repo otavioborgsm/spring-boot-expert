a57234d1092f898372a241fd117817a3ccfccbbe
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-10 11:54:07 -0300

    query methods

--- vendas/src/main/java/io/github/otavioborgsm/VendasApplication.java
+++ vendas/src/main/java/io/github/otavioborgsm/VendasApplication.java
@@ -1,11 +1,32 @@
 package io.github.otavioborgsm;
-
+import io.github.otavioborgsm.domain.entity.Cliente;
+import io.github.otavioborgsm.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.save(new Cliente("Otávio"));
+            clientes.save(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.findAll();
+            todosClientes.forEach(System.out::println);
+
+            List<Cliente> result = clientes.encontrarPorNome("Otávio");
+            result.forEach(System.out::println);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
--- vendas/src/main/java/io/github/otavioborgsm/domain/repositorio/Clientes.java
+++ vendas/src/main/java/io/github/otavioborgsm/domain/repositorio/Clientes.java
@@ -1,57 +1,17 @@
 package io.github.otavioborgsm.domain.repositorio;
 
 import io.github.otavioborgsm.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
-@Repository
-public class Clientes {
 
-    @Autowired
-    private EntityManager entityManager;
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Transactional
-    public Cliente salvar(Cliente cliente){
-        entityManager.persist(cliente);
-        return cliente;
-    }
+    @Query(value = " select * from Cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> encontrarPorNome( @Param("nome") String nome );
 
-    @Transactional
-    public Cliente atualizar(Cliente cliente){
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente){
-        if(!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id){
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome){
-        String jpql = " select c from Cliente c where c.nome like :nome ";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional
-    public List<Cliente> obterTodos(){
-        return entityManager.createQuery(" from Cliente ", Cliente.class).getResultList();
-    }
-
+    boolean existsByNome(String nome);
 }
