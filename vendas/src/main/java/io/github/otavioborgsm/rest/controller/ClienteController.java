790f98164337274319aedfc7a1f51a0bd664054d
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-13 18:11:29 -0300

    post e get do cliente

---
+++ vendas/src/main/java/io/github/otavioborgsm/rest/controller/ClienteController.java
@@ -0,0 +1,37 @@
+package io.github.otavioborgsm.rest.controller;
+
+import io.github.otavioborgsm.domain.entity.Cliente;
+import io.github.otavioborgsm.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ClienteController {
+
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save( @RequestBody Cliente cliente ){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+}
