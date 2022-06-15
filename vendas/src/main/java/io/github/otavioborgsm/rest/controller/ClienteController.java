216a99664823b9c163948a41b7760f44d44b3ef3
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-15 13:18:50 -0300

    controller de produtos

--- vendas/src/main/java/io/github/otavioborgsm/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/otavioborgsm/rest/controller/ClienteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes/")
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     private Clientes clientes;
