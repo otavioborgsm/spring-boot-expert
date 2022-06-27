c21a4ceff5dcf6333d648662261cc06d998f036e
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-27 11:44:57 -0300

    mapeando entidade cidade

--- localizacao/src/main/java/io/github/otavioborgsm/localizacao/LocalizacaoApplication.java
+++ localizacao/src/main/java/io/github/otavioborgsm/localizacao/LocalizacaoApplication.java
@@ -1,10 +1,16 @@
 package io.github.otavioborgsm.localizacao;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocalizacaoApplication {
+public class LocalizacaoApplication implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Inicializando!!");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LocalizacaoApplication.class, args);
---
+++ localizacao/src/main/java/io/github/otavioborgsm/localizacao/domain/entity/Cidade.java
@@ -0,0 +1,24 @@
+package io.github.otavioborgsm.localizacao.domain.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_cidade")
+@Data
+public class Cidade {
+
+    @Id
+    @Column(name = "id_cidade")
+    private Long id;
+
+    @Column(name = "nome", length = 50)
+    private String nome;
+
+    @Column(name = "qtd_habitantes")
+    private Long habitantes;
+}
