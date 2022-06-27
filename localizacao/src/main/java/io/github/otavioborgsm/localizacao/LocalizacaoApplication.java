e55d6ff9219cf02796832bfd0bd1ec0223c91afd
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-27 12:03:47 -0300

    repositório de cidades testado

--- localizacao/src/main/java/io/github/otavioborgsm/localizacao/LocalizacaoApplication.java
+++ localizacao/src/main/java/io/github/otavioborgsm/localizacao/LocalizacaoApplication.java
@@ -1,15 +1,34 @@
 package io.github.otavioborgsm.localizacao;
 
+import io.github.otavioborgsm.localizacao.domain.entity.Cidade;
+import io.github.otavioborgsm.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Inicializando!!");
+        salvarCidade();
+        listarCidades();
+    }
+
+    @Transactional
+    void salvarCidade(){
+        var cidade = new Cidade(1L, "Torres", 40000L);
+        cidadeRepository.save(cidade);
+    }
+
+    void listarCidades(){
+        cidadeRepository.findAll().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
--- localizacao/src/main/java/io/github/otavioborgsm/localizacao/domain/entity/Cidade.java
+++ localizacao/src/main/java/io/github/otavioborgsm/localizacao/domain/entity/Cidade.java
@@ -1,6 +1,8 @@
 package io.github.otavioborgsm.localizacao.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_cidade")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cidade {
 
     @Id
