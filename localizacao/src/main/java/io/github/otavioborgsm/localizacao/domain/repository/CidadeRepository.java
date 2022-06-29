29235374064fcff58c2f405aa86ca6f6f76dcb31
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-29 16:27:19 -0300

    filtro dinâmico de pesquisa e consultas ordenadas e paginadas

--- localizacao/src/main/java/io/github/otavioborgsm/localizacao/domain/repository/CidadeRepository.java
+++ localizacao/src/main/java/io/github/otavioborgsm/localizacao/domain/repository/CidadeRepository.java
@@ -1,6 +1,9 @@
 package io.github.otavioborgsm.localizacao.domain.repository;
 
 import io.github.otavioborgsm.localizacao.domain.entity.Cidade;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,13 +14,17 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
     // busca pelo nome correto
     List<Cidade> findByNome(String nome);
 
-    // busca pelo nome like
+    // busca pelo nome like ordenado
     // "a" exatamente a string
     // "A%" começa com A
     // "%a%" contem a na string
     // "%a" termina com a
     @Query(" select c from Cidade c where upper(c.nome) like upper(?1) ")
-    List<Cidade> findByNomeLike(String nome);
+    List<Cidade> findByNomeLike(String nome, Sort sort);
+
+    // busca pelo nome like paginado
+    @Query(" select c from Cidade c where upper(c.nome) like upper(?1) ")
+    Page<Cidade> findByNomeLike(String nome, Pageable pageable);
 
     // começando por aquele pedaço
     List<Cidade> findByNomeStartingWith(String nome);
--- localizacao/src/main/java/io/github/otavioborgsm/localizacao/service/CidadeService.java
+++ localizacao/src/main/java/io/github/otavioborgsm/localizacao/service/CidadeService.java
@@ -2,8 +2,14 @@ package io.github.otavioborgsm.localizacao.service;
 
 import io.github.otavioborgsm.localizacao.domain.entity.Cidade;
 import io.github.otavioborgsm.localizacao.domain.repository.CidadeRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CidadeService {
@@ -19,8 +25,13 @@ public class CidadeService {
                 .forEach(System.out::println);
     }
 
-    public void listaCidadePorNome(String nome){
-        repository.findByNomeLike(nome)
+    public void listaCidadePorNomeOrdenado(String nome, Sort sort){
+        repository.findByNomeLike( nome, sort )
+                .forEach(System.out::println);
+    }
+
+    public void listaCidadePorNomePaginado(String nome, Pageable pageable){
+        repository.findByNomeLike( nome, pageable )
                 .forEach(System.out::println);
     }
 
@@ -38,5 +49,11 @@ public class CidadeService {
     public void listarCidades(){
         repository.findAll()
                 .forEach(System.out::println);
+    }
+
+    public List<Cidade> filtroDinamico( Cidade cidade ){
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase();
+        Example<Cidade> example = Example.of(cidade, matcher);
+        return repository.findAll(example);
     }
 }
