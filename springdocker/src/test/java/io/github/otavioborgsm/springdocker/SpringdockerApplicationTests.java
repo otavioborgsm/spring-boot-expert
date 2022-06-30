ecbecda45a95ad2946f54433a3d586cbe5a29ebd
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-06-30 17:33:47 -0300

    springdocker

---
+++ springdocker/src/test/java/io/github/otavioborgsm/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.otavioborgsm.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
